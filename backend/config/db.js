02b441b1de7fa6135e4cb8e7eb6ddefec4f1b02d
Rukwaro <irenerukwaro9@gmail.com>
2024-12-14 03:04:58 +0300

    Migrated DB to Azure Database and configured

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+const fs = require('fs');   
+const path = require('path');
 require('dotenv').config();
+
+const cert = path.join(__dirname, process.env.DB_SSL_CERT);
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {ca: fs.readFileSync(cert)}
 });
 
 module.exports = pool.promise();
