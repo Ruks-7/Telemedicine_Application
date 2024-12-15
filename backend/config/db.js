bc935936f92e5e9d6c45865011c4652f0975d9f3
Rukwaro <irenerukwaro9@gmail.com>
2024-12-15 15:22:12 +0300

    Attempt to fix deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,8 +8,7 @@ const pool = mysql.createPool(
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {ca: fs.readFileSync(process.env.DB_SSL_CERT, "utf8")}
+    database: process.env.DB_NAME
 });
 
 module.exports = pool.promise();
