ecbeb8465d5ecf04d37d96a1c9c6e260ef822631
Rukwaro <irenerukwaro9@gmail.com>
2024-12-15 14:50:52 +0300

    Attempt to fix deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,14 +4,14 @@ const path = require('path');
 require('dotenv').config();
 
 const cert = path.join(__dirname + process.env.DB_SSL_CERT);
-const ssl_cert = [fs.readFileSync(cert, "utf8")];
 
-const pool = mysql.createPool({
+const pool = mysql.createPool(
+    {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {ca: ssl_cert}
+    ssl: {ca: fs.readFileSync(cert, "utf8")}
 });
 
 module.exports = pool.promise();
